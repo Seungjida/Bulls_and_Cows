d1e4f0c6aa639333e2b73f058dfc016fa71e13ce
Seungjida <esg0107@pusan.ac.kr>
2023-05-08 15:05:49 +0900

    Update number

---
+++ number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+        int number;
+        printf("Input 3 digits number : ");
+        scanf("%d", &number);
+        printf("Your number is %d\n", number);
+        return 0;
+}
